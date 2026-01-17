ab65bf3261e9b66023691349abd48e12b1887f9f
Atharv Tiwari <atharvtiwari696969@gmail.com>
2026-01-17 09:43:55 +0530

    Python

--- Type-1.py
+++ Type-1.py
@@ -1,9 +1,20 @@
+""" Task Requirements
+Use a for loop to go from 1 to 20.
+
+Inside the loop:
+
+If the number is a multiple of 5 → skip it using continue.
+
+If the number equals 17 → stop the loop using break.
+
+Print the numbers that remain. 
+"""
+# CODE BEGINS HERE
+
 Num = 0
-while True:
-    Num += 1
-    print(Num)
-    if Num %3 == 0:
+for Num in range (1 , 21):
+    if Num %5 == 0:
         continue
-    if Num == 15:
-        break 
-        print("Program Finished Successfully.")
+    if Num == 17:
+        break
+    print(Num)
